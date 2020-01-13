636705ca152e925ffa2ebf1783222353163dd069
sobuk <bear064@naver.com>
2020-01-13 18:27:30 +0900

    add nodejs 19 file

--- nodejs/opentutorial/main.js
+++ nodejs/opentutorial/main.js
@@ -5,8 +5,10 @@ var url = require('url');
 var app = http.createServer((request, response) => {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log('_url', _url);
+    var pathname = url.parse(_url, true).pathname;
+    // var title = queryData.id;
+
+    /*console.log('_url', _url);
     console.log('queryData.id', queryData.id);
     if (_url == '/') {
         // _url = '/index.html';
@@ -16,9 +18,17 @@ var app = http.createServer((request, response) => {
         return response.writeHead(404);
     }
     response.writeHead(200);
-    fs.readFile(`data/${queryData.id}.txt`, 'utf-8', (err, description) => {
-        // if(err) throw err;
-        var template = `
+     */
+
+    if (pathname === '/') {
+        // console.log('url.parse(_url, true)', url.parse(_url, true))
+
+        if (queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}.txt`, 'utf-8', (err, description) => {
+                // if(err) throw err;
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var template = `
         <!doctype html>
         <html>
         <head>
@@ -36,9 +46,43 @@ var app = http.createServer((request, response) => {
           <p>${description}</p>
         </body>
         </html>
-    `;
-        response.end(template);
-    });
+        `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}.txt`, 'utf-8', (err, description) => {
+                // if(err) throw err;
+                var title = queryData.id;
+                var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+
+    }
 
     // response.end(fs.readFileSync(__dirname + _url));
     // response.end(queryData.id);
